f9c2511759c263761ab54c011c5d52c50a0a4860
ellen <ellentyyu@gmail.com>
2023-12-09 23:13:54 +0800

    feat: 移除website資料夾/新增block元件/調整navbar樣式

---
+++ src/components/Homepage/Block.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Block() {
+    return (
+        <section className='block'>
+            <div className='container'>
+                <p>hhhheeeyyyyyy</p>
+            </div>
+        </section>
+    );
+}
+
+export default Block;
